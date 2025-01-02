5a9152c9c65ddb3b30d2241ef3f290ff07f8f363
DevCabalceta <cabalceta.gabriel.2001@gmail.com>
2025-01-01 23:42:13 -0600

    Actualizacion

--- iaArt/js/app.js
+++ iaArt/js/app.js
@@ -1,6 +1,17 @@
+const toastLiveExample = document.getElementById('liveToast')
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+toastBootstrap.show()
+
+
+//PopOver
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+//Inicializacion del componente carousel
 const myCarouselElement = document.querySelector('#carouseliaArt')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
